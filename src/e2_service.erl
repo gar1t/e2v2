17b7911338905bc09cb50e9b42950f5b46582966
garrett <g@rre.tt>
2012-01-06 14:53:45 -0600

    Misc cleanup of service

--- src/e2_service.erl
+++ src/e2_service.erl
@@ -7,14 +7,11 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--define(SERVER, ?MODULE).
+-export([behaviour_info/1]).
 
--record(state, {mod,
-                cb_state,
-                init,
-                handle_info,
-                terminate,
-                timeout_cb}).
+behaviour_info(callbacks) -> [{handle_msg, 3}].
+
+-record(state, {mod, cb_state, first_msg}).
 
 %%%===================================================================
 %%% API
@@ -50,7 +47,9 @@ cast(ServerRef, Handler) ->
 %%%===================================================================
 
 init({Module, Args}) ->
-    dispatch_init(init_state(Module), Args).
+    State = init_state(Module),
+    maybe_trap_exit(State),
+    dispatch_init(Module, Args, State).
 
 handle_call({'$call', Handler}, From, State) ->
     dispatch_call(Handler, From, State).
@@ -88,26 +87,33 @@ start_gen_server({registered, Name}, Module, Args, Options) ->
     gen_server:start_link(
       {local, Name}, ?MODULE, {Module, Args}, gen_server_options(Options)).
 
+%% TODO: What do we want to pass through to gen_server?
 gen_server_options(_Options) -> [].
 
 init_state(Module) when is_atom(Module) ->
-    #state{mod=Module,
-           init=e2_util:optional_function(Module, init, 1),
-           handle_info=e2_util:optional_function(Module, handle_info, 2),
-           terminate=e2_util:optional_function(Module, terminate, 2)}.
+    #state{mod=Module}.
 
-dispatch_init(#state{init=undefined}=State, Args) ->
-    handle_init_result({ok, Args}, State);
-dispatch_init(#state{init=Init}=State, Args) ->
-    handle_init_result(e2_util:apply_handler(Init, [Args]), State).
+maybe_trap_exit(#state{mod=Mod}) ->
+    case e2_util:optional_function(Mod, terminate, 2) of
+        undefined -> ok;
+        _ -> process_flag(trap_exit, true)
+    end.
+
+dispatch_init(Module, Args, State) ->
+    case e2_util:optional_function(Module, init, 1) of
+        undefined ->
+            handle_init_result({ok, Args}, State);
+        Init ->
+            handle_init_result(e2_util:apply_handler(Init, [Args]), State)
+    end.
 
 handle_init_result({ok, CbState}, State) ->
     {ok, set_cb_state(State, CbState)};
-handle_init_result({ok, CbState, {_Function, _Args}=Callback}, State) ->
-    {ok, set_timeout_cb(set_cb_state(State, CbState), Callback), 0};
 handle_init_result({ok, CbState, Timeout}, State)
   when is_integer(Timeout) ->
     {ok, set_cb_state(State, CbState), Timeout};
+handle_init_result({ok, CbState, FirstMsg}, State) ->
+    {ok, set_first_msg(set_cb_state(State, CbState), FirstMsg), 0};
 handle_init_result({stop, Reason}, _State) ->
     {stop, Reason};
 handle_init_result(ignore, _State) ->
@@ -115,12 +121,8 @@ handle_init_result(ignore, _State) ->
 handle_init_result(Other, _State) ->
     exit({bad_return_value, Other}).
 
-dispatch_call({Function, Args}, From,
-              #state{mod=Module, cb_state=CbState}=State)
-              when is_atom(Function) ->
-    handle_call_result(
-      e2_util:apply_handler({Module, Function, Args}, [From, CbState]),
-      State).
+dispatch_call(Msg, From, #state{mod=Module, cb_state=CbState}=State) ->
+    handle_call_result(dispatch_msg(Module, Msg, From, CbState), State).
 
 handle_call_result({reply, Reply, CbState}, State) ->
     {reply, Reply, set_cb_state(State, CbState)};
@@ -137,49 +139,60 @@ handle_call_result({stop, Reason, Reply, CbState}, State) ->
 handle_call_result(Other, _State) ->
     exit({bad_return_value, Other}).
 
-dispatch_cast({Function, Args},
-              #state{mod=Module, cb_state=CbState}=State)
-              when is_atom(Function) ->
-    handle_cast_result(
-      e2_util:apply_handler({Module, Function, Args}, [CbState]),
-      State).
+dispatch_cast(Msg, #state{mod=Module, cb_state=CbState}=State) ->
+    handle_cast_result(dispatch_msg_noreply(Module, Msg, CbState), State).
 
 handle_cast_result({noreply, CbState}, State) ->
     {noreply, set_cb_state(State, CbState)};
+handle_cast_result({noreply, CbState, Timeout}, State) ->
+    {noreply, set_cb_state(State, CbState), Timeout};
+handle_cast_result({reply, _Reply, CbState}, State) ->
+    {noreply, set_cb_state(State, CbState)};
+handle_cast_result({reply, _Reply, CbState, Timeout}, State) ->
+    {noreply, set_cb_state(State, CbState), Timeout};
 handle_cast_result({stop, Reason, CbState}, State) ->
     {stop, Reason, set_cb_state(State, CbState)};
 handle_cast_result(Other, _State) ->
     exit({bad_return_value, Other}).
 
 dispatch_info(timeout, #state{mod=Module, cb_state=CbState,
-                              timeout_cb={Function, Args}}=State) ->
+                              first_msg=Msg}=State) when Msg =/= undefined ->
     handle_info_result(
-      e2_util:apply_handler({Module, Function, Args}, [CbState]),
-      clear_timeout_cb(State));
-dispatch_info(_Msg, #state{handle_info=undefined}=State) ->
-    {noreply, State};
-dispatch_info(Msg, #state{handle_info=Handler, cb_state=CbState}=State) ->
-    handle_info_result(e2_util:apply_handler(Handler, [Msg, CbState]), State).
+      dispatch_msg_noreply(Module, Msg, CbState), clear_first_msg(State));
+dispatch_info(Msg, #state{mod=Module, cb_state=CbState}=State) ->
+    handle_info_result(dispatch_msg_noreply(Module, Msg, CbState), State).
 
 handle_info_result({noreply, CbState}, State) ->
     {noreply, set_cb_state(State, CbState)};
 handle_info_result({noreply, CbState, Timeout}, State) ->
+    {noreply, set_cb_state(State, CbState), Timeout};
+handle_info_result({reply, _Reply, CbState}, State) ->
+    {noreply, set_cb_state(State, CbState)};
+handle_info_result({reply, _Reply, CbState, Timeout}, State) ->
     {noreply, set_cb_state(State, CbState), Timeout};
 handle_info_result({stop, Reason, CbState}, State) ->
     {stop, Reason, set_cb_state(State, CbState)};
 handle_info_result(Other, _State) ->
     exit({bad_return_value, Other}).
 
-dispatch_terminate(_Reason, #state{terminate=undefined}) ->
-    ok;
-dispatch_terminate(Reason, #state{terminate=Handler, cb_state=CbState}) ->
-    e2_util:apply_handler(Handler, [Reason, CbState]).
+dispatch_terminate(Reason, #state{mod=Mod, cb_state=CbState}) ->
+    case e2_util:optional_function(Mod, terminate, 2) of
+        undefined -> ok;
+        Terminate ->
+            e2_util:apply_handler(Terminate, [Reason, CbState])
+    end.
+
+dispatch_msg_noreply(Module, Msg, CbState) ->
+    Module:handle_msg(Msg, noreply, CbState).
+
+dispatch_msg(Module, Msg, From, CbState) ->
+    Module:handle_msg(Msg, From, CbState).
 
 set_cb_state(State, CbState) ->
     State#state{cb_state=CbState}.
 
-set_timeout_cb(State, Callback) ->
-    State#state{timeout_cb=Callback}.
+set_first_msg(State, FirstMsg) ->
+    State#state{first_msg=FirstMsg}.
 
-clear_timeout_cb(State) ->
-    State#state{timeout_cb=undefined}.
+clear_first_msg(State) ->
+    State#state{first_msg=undefined}.
