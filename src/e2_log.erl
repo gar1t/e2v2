7597d844f280b8968cd4682569f487d7baa93da7
garrett <g@rre.tt>
2012-01-23 18:29:15 -0600

    Simplify the stupid log API

--- src/e2_log.erl
+++ src/e2_log.erl
@@ -1,22 +1,15 @@
 -module(e2_log).
 
--export([info/1, info/2, info_report/1,
-         error/1, error/2, error_report/1]).
+-export([info/1, info/2, error/1, error/2]).
 
 info(Msg) ->
-    error_logger:info_msg(Msg).
+    error_logger:info_report(Msg).
 
 info(Msg, Args) ->
-    error_logger:info_msg(Msg, Args).
-
-info_report(Report) ->
-    error_logger:info_report(Report).
+    error_logger:info_report(io_lib:format(Msg, Args)).
 
 error(Msg) ->
-    error_logger:error_msg(Msg).
+    error_logger:error_report(Msg).
 
 error(Msg, Args) ->
-    error_logger:error_msg(Msg, Args).
-
-error_report(Report) ->
-    error_logger:error_report(Report).
+    error_logger:error_report(io_lib:format(Msg, Args)).
