56f1088047b151cbfd071039d6c18dad33dc9a8e
garrett <g@rre.tt>
2012-01-24 09:26:11 -0600

    Pass through tty

--- src/e2_log.erl
+++ src/e2_log.erl
@@ -1,6 +1,6 @@
 -module(e2_log).
 
--export([info/1, info/2, error/1, error/2]).
+-export([info/1, info/2, error/1, error/2, tty/1]).
 
 info(Msg) ->
     error_logger:info_report(Msg).
@@ -13,3 +13,6 @@ error(Msg) ->
 
 error(Msg, Args) ->
     error_logger:error_report(io_lib:format(Msg, Args)).
+
+tty(Flag) ->
+    error_logger:tty(Flag).
